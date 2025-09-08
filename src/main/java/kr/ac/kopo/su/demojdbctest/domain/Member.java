139e29bf99f2ab7925babf7c330da021c3b105ae
Mourn5367 <qwerrrt6@gmail.com>
2025-09-08 11:10:51 +0900

    JPA 기본 메소드 사용 CRUD

---
+++ src/main/java/kr/ac/kopo/su/demojdbctest/domain/Member.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.su.demojdbctest.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Member
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+    private int age;
+    private String email;
+}
---
+++ src/main/java/kr/ac/kopo/su/demojdbctest/repository/MemberRepository02.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.su.demojdbctest.repository;
+
+import kr.ac.kopo.su.demojdbctest.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository02 extends JpaRepository<Member, Integer>
+{
+
+}
